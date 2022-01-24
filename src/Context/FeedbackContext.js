22923b321d6fab7c08d094ce9f1b140dbda1b82d
Mujahid-Developer <mujahidulislam575@gmail.com>
2022-01-24 22:58:51 +0600

    Added a moc back end

--- src/Context/FeedbackContext.js
+++ src/Context/FeedbackContext.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from "uuid";
 import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
@@ -18,19 +17,31 @@ export const FeedbackProvider = ({ children }) => {
   // Fetch Feedback
 
   const fetchFeedback = async ()=>{
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
     const data = await response.json()
     setFeedback(data)
     setIsLoading(false)
   }
 
   // Add Feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+      const data = await response.json()
+
+    setFeedback([data, ...feedback]);
   };
   // Delete Feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async(id) => {
+    await fetch(`/feedback/${id}`, {
+      method: "DELETE"
+    })
     // if (window.confirm("are you sure want to delete?")) {
     //   setFeedback(feedback.filter((item) => item.id !== id));
     // }
@@ -45,8 +56,19 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Update Feedback Item
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) =>(item.id === id ? {...item, ...updItem } : item))
+  const updateFeedback = async(id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": 'application/json',
+      },
+      body: JSON.stringify(updItem)
+    })
+
+    const data = await response.json()
+
+
+    setFeedback(feedback.map((item) =>(item.id === id ? {...item, ...data } : item))
     );
   };
 
